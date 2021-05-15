21159226aba14634f5e475a57fb8816e2af89a36
Ammar Bohra <bohraammar@gmail.com>
2021-05-15 20:26:45 +0530

    layout-xl, sidebar, identity, support-artist

---
+++ js/script.js
@@ -0,0 +1,11 @@
+//setting margin top and bottom depending on margin left
+// const wrapper = document.querySelector('.wrapper');
+// function wrapperSetMargin() {
+//   wrapper.style.marginTop = `${Math.min(wrapper.offsetLeft, 192)}px`;
+//   wrapper.style.marginBottom = `${Math.min(wrapper.offsetLeft, 192)}px`;
+// }
+// if(wrapper && window.innerWidth >= 1536) {
+//   wrapperSetMargin();
+//   window.addEventListener('resize', wrapperSetMargin)
+// }
+
