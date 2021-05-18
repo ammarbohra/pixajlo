f1d2348aae73da0d78365ab3d60d865126a32add
Ammar Bohra <bohraammar@gmail.com>
2021-05-18 22:46:42 +0530

    some modals added

--- js/script.js
+++ js/script.js
@@ -1,11 +1,27 @@
-//setting margin top and bottom depending on margin left
-// const wrapper = document.querySelector('.wrapper');
-// function wrapperSetMargin() {
-//   wrapper.style.marginTop = `${Math.min(wrapper.offsetLeft, 192)}px`;
-//   wrapper.style.marginBottom = `${Math.min(wrapper.offsetLeft, 192)}px`;
-// }
-// if(wrapper && window.innerWidth >= 1536) {
-//   wrapperSetMargin();
-//   window.addEventListener('resize', wrapperSetMargin)
-// }
+const modals = document.querySelectorAll('.modal');
+const gridItems = document.querySelector('.grid').children;
+
+const closeBtn = document.querySelector('.close-btn');
+closeBtn.addEventListener('click', function() {
+  gsap.to(modals, {autoAlpha: 0})
+  gsap.to(closeBtn, {autoAlpha: 0})
+  for(let i = 0; i < 9; i++) {
+    gsap.to(gridItems[i], {autoAlpha: 1})
+  }
+});
+const modalBtns = document.querySelectorAll('[modal]');
+modalBtns.forEach(function(modalBtn) {
+  modalBtn.addEventListener('click', function() {
+    let modalClasses = modalBtn.getAttribute('modal').split(" ");
+    let modals = [];
+    modalClasses.forEach(function(modalClass) {
+      modals.push(document.querySelector(modalClass));
+    });
+    gsap.to(modals, {autoAlpha: 1})
+    gsap.to(closeBtn, {autoAlpha: 1})
+    for(let i = 0; i < 9; i++) {
+      gsap.to(gridItems[i], {autoAlpha: 0})
+    }
+  });
+});
 
